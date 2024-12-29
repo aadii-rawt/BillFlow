523529553c3192c519dd6f91700cb6e9f4937e1f
adityarawat4 <rawatadii060@gmail.com>
2024-12-29 23:43:22 +0530

    c

--- client/src/pages/Vendors.jsx
+++ client/src/pages/Vendors.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FiPlus } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+import VendorProfile from '../components/VendorProfile'
 
 function Vendors() {
+    const [vendorProfile,setVendorProfile] = useState(true)
     return (
-        <div className=''>
+        <div className='relative'>
             <div className=' p-4 flex items-center justify-between'>
                 <div>
                     <button className='text-2xl font-medium'>All Vendors</button>
@@ -14,11 +16,10 @@ function Vendors() {
                 </div>
             </div>
             <div>
-                <table className='bg-gray-100 w-full border-y'>
+                <table className=' w-full'>
                     <thead>
-                        <tr className='text-gray-500 !font-normal'>
+                        <tr className='text-gray-500 bg-gray-100 !font-normal border-y'>
                             <th className='font-medium text-sm py-2'>NAME</th>
-                            <th className='font-medium text-sm py-2'>COMPANY NAME</th>
                             <th className='font-medium text-sm py-2'>COMPANY NAME</th>
                             <th className='font-medium text-sm py-2'>EMAIL</th>
                             <th className='font-medium text-sm py-2'>WORK PHONE</th>
@@ -26,8 +27,36 @@ function Vendors() {
                             <th className='font-medium text-sm py-2'>UNUSED CREDITS</th>
                         </tr>
                     </thead>
+                    <tbody>
+                        <tr className='text-center cursor-pointer hover:bg-gray-100 border-b' onClick={() => setVendorProfile(true)}>
+                            <td className='text-sm py-2.5 font-medium text-blue-500'>Aditya</td>
+                            <td className='text-sm py-2.5'>Tech Tree Pvt. Ltd.</td>
+                            <td className='text-sm py-2.5'>aditya@email.com</td>
+                            <td className='text-sm py-2.5'>+91 7849474933</td>
+                            <td className='text-sm py-2.5 font-medium'>₹0.00</td>
+                            <td className='text-sm py-2.5 font-medium'>₹0.00</td>
+                        </tr>
+                        <tr className='text-center cursor-pointer hover:bg-gray-100 border-b'>
+                            <td className='text-sm py-2.5 font-medium text-blue-500'>vinay</td>
+                            <td className='text-sm py-2.5'>google</td>
+                            <td className='text-sm py-2.5'>vinay@mt.com</td>
+                            <td className='text-sm py-2.5'>+91 79274793888</td>
+                            <td className='text-sm py-2.5 font-medium'>₹0.00</td>
+                            <td className='text-sm py-2.5 font-medium'>₹0.00</td>
+                        </tr>
+                        <tr className='text-center cursor-pointer hover:bg-gray-100 border-b'>
+                            <td className='text-sm py-2.5 font-medium text-blue-500'>sahil</td>
+                            <td className='text-sm py-2.5'>apple</td>
+                            <td className='text-sm py-2.5'>aditya@email.com</td>
+                            <td className='text-sm py-2.5'>+91 89890000789</td>
+                            <td className='text-sm py-2.5 font-medium'>₹0.00</td>
+                            <td className='text-sm py-2.5 font-medium'>₹0.00</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
+
+            {vendorProfile && <VendorProfile setVendorProfile={setVendorProfile} />}
         </div>
     )
 }
