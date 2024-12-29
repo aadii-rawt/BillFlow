2742fc69f0c2185e1ed77e2c05282708262e95d7
adityarawat4 <rawatadii060@gmail.com>
2024-12-29 23:42:46 +0530

    commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { IoIosSearch } from "react-icons/io";
 
 function Header() {
   return (
-    <div className='bg-[#21263c]  px-5 py-3 flex items-center justify-end'>
-        {/* <div>
-            <h1 className='font-semibold text-xl text-white'>BillFlow</h1>
-        </div> */}
-        <div>
-            <div className='w-10 h-10 rounded-full bg-green-700 text-white flex items-center justify-center font-semibold'>A</div>
+    <div className='bg-[#21263c]  px-5 py-3 flex items-center justify-between'>
+      <div>
+        <div className='border rounded-md  py-1 flex items-center'>
+          <IoIosSearch className='text-gray-300 mx-2' />
+          <input type="text" placeholder='Search in Bills' className='outline-none bg-transparent text-white px-2 py-0.5 border-l !border-gray-600' />
         </div>
+      </div>
+      <div>
+        <div className='w-10 h-10 cursor-pointer rounded-full bg-green-600 text-white flex items-center justify-center font-medium text-xl'>A</div>
+      </div>
     </div>
   )
 }
--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,12 +1,13 @@
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function Modal({ children }) {
-    return ReactDOM?.createPortal(
-        <div>
+    return ReactDOM.createPortal(
+        <div className='bg-black/50 z-50 max-h-screen overflow-hidden fixed top-0 left-0 w-full flex items-center justify-center mb-5'>
             {children}
         </div>,
         document.getElementById("portal")
-    )
+    );
 }
 
-export default Modal
+export default Modal;
