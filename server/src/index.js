db51d3203ffcb71e5a0110f6e32736f5b4aba75a
adityarawat4 <rawatadii060@gmail.com>
2025-01-06 11:38:47 +0530

    c

---
+++ server/src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+
+const vendorRoutes = require("../routes/vendor");
+app.use(
+  cors({
+    origin: "http://localhost:5174", // Allow requests from your frontend
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    // allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
+  })
+);
+
+app.use(express.json());
+
+app.use(bodyParser.json());
+app.use("/vendor", vendorRoutes);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("Server listening on port", PORT);
+});
