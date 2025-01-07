176c5e57f2e2a397860783069e02f61c27c480bd
adityarawat4 <rawatadii060@gmail.com>
2025-01-07 14:20:22 +0530

    c

--- server/db/index.js
+++ server/db/index.js
@@ -1,20 +1,24 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://rawatadii060:yDuEBbm5Ah20hjNt@cluster0.0yiiy.mongodb.net/BillFlow");
+mongoose.connect(
+  "mongodb+srv://rawatadii060:yDuEBbm5Ah20hjNt@cluster0.0yiiy.mongodb.net/BillFlow"
+);
 
 const vendorsSchema = new mongoose.Schema({
-    salutation: String,
-    firstName: String,
-    lastName: String,
-    companyName: String,
-    displayName: String,
-    email: String,
-    Phone: String,
+  userId: String,
+  vendors: [
+    {
+      salutation: String,
+      firstName: String,
+      lastName: String,
+      companyName: String,
+      displayName: String,
+      email: String,
+      Phone: String,
+    },
+  ],
 });
 
+const vendors = mongoose.model("vendors", vendorsSchema);
 
-const vendors = mongoose.model("vendors",vendorsSchema)
-
-
-
-module.exports = {vendors}
+module.exports = { vendors };
