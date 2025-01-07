bd61e1f4e75c78fd9cf69125ed2f7ad1b509b6e6
adityarawat4 <rawatadii060@gmail.com>
2025-01-07 14:20:18 +0530

    c

--- server/routes/vendor.js
+++ server/routes/vendor.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.post("/newvendor", async (req, res) => {
   const {
+    userId,
     salutation,
     lastName,
     firstName,
@@ -13,19 +14,51 @@ router.post("/newvendor", async (req, res) => {
     Phone,
   } = req.body;
 
-  await vendors.create({
-    salutation,
-    lastName,
-    firstName,
-    companyName,
-    displayName,
-    email,
-    Phone,
-  });
+  try {
+    const userVendors = await vendors.findOne({ userId });
 
-  res.json({
-    msg: "Vendor Created Successfully",
-  });
+    if (!userVendors) {
+      userVendors = await vendors.create({
+        userId,
+        vendors: [],
+      });
+    }
+
+    userVendors.vendors.push({
+      salutation,
+      lastName,
+      firstName,
+      companyName,
+      displayName,
+      email,
+      Phone,
+    });
+
+    await userVendors.save();
+
+    res.json({ msg: "Vendor created successfully" });
+  } catch (error) {
+    res.json({ msg: "Error creating vendor" });
+  }
+
+  // await vendors.create({
+  //   userId,
+  //   vendors : [
+  //     {
+  //       salutation,
+  //       lastName,
+  //       firstName,
+  //       companyName,
+  //       displayName,
+  //       email,
+  //       Phone,
+  //     }
+  //   ]
+  // });
+
+  // res.json({
+  //   msg: "Vendor Created Successfully",
+  // });
 });
 
 router.get("/vendors", (req, res) => {
