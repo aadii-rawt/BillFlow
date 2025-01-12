868cedaf9af50de44a0049bbb5924f876f464a2f
adityarawat4 <rawatadii060@gmail.com>
2025-01-12 20:31:21 +0530

    bill slice

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,10 @@
 import {configureStore, combineReducers } from "@reduxjs/toolkit"
 import vendorSlice from './slices/vendorSlice'
+import billSlice from './slices/billSlice'
 
 const reducer = combineReducers({
-    vendorSlice
+    vendorSlice,
+    billSlice,
 })
 const store = configureStore({
     reducer
