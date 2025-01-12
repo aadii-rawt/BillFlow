36200ee5d45634483ade05e8d294d327313b6c7c
adityarawat4 <rawatadii060@gmail.com>
2025-01-12 20:32:43 +0530

    vendor slice

--- client/src/store/slices/vendorSlice.js
+++ client/src/store/slices/vendorSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const vendorSlice = createSlice({
-    name : "vendosr",
+    name : "vendors",
     initialState : {
         vendors : [
             {
