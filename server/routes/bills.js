df985f3733b83613955e21da1c9d2cb2aaf9bc0a
adityarawat4 <rawatadii060@gmail.com>
2025-01-12 20:33:02 +0530

    c

---
+++ server/routes/bills.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { Bills } = require("../db");
+const router = express.Router();
+
+router.post("/newbill", async (req, res) => {
+  const { userId } = req.body;
+  console.log(req.body);
+  
+
+  try {
+    const userBills = await Bills.findOne({ userId });
+    if (!userBills) {
+      userBills = await Bills.create({
+        userId,
+        bills: [],
+      });
+    }
+
+    userBills.bills.push({
+     ...req.body
+    });
+
+    await userBills.save();
+
+    res.json({ msg: "bill created successfully" });
+  } catch (error) {
+    res.json({ msg: "Error creating bill" });
+  }
+});
+
+module.exports = router;
