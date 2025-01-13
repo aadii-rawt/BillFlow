737854066df9269d463fb0a1908e8f84b5af4bc9
adityarawat4 <rawatadii060@gmail.com>
2025-01-13 12:52:09 +0530

    c

--- client/src/store/slices/stateSlice.js
+++ client/src/store/slices/stateSlice.js
@@ -5,6 +5,7 @@ const stateSlice = createSlice({
   initialState: {
     state: [],
     vendorProfile: null,
+    billPreview : null
   },
   reducers: {
     setVendorProfile(state, action) {
@@ -13,8 +14,14 @@ const stateSlice = createSlice({
     closeVendorProfile(state, action) {
       state.vendorProfile = null;
     },
+    setBillPreview(state, action) {
+      state.billPreview = action.payload;
+    },
+    closeBillPreview(state) {
+      state.billPreview = null;
+    },
   },
 });
 
-export const { setVendorProfile, closeVendorProfile } = stateSlice.actions;
+export const { setVendorProfile, closeVendorProfile,setBillPreview,closeBillPreview } = stateSlice.actions;
 export default stateSlice.reducer;
