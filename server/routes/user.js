1c34a5381e4b2afe5eaed53219a78957e7f304dc
adityarawat4 <rawatadii060@gmail.com>
2025-01-18 14:32:53 +0530

    create the user route

---
+++ server/routes/user.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { Users } = require("../db");
+const router = express.Router()
+
+router.post("/signup", async (req,res) => {
+    console.log(req.body);
+    const email =  req.body.email
+    try {
+        const isUserExist = await Users.findOne({email})
+        console.log(isUserExist);
+        
+        if(isUserExist){
+            return res.status(409).send({
+                msg: "User with this email already exists."
+            });
+        }
+
+        await Users.create(
+            {
+                ...req.body
+            }
+        )
+
+        res.send({
+            msg : "user created successfully"
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.send({
+            "msg" : "Error can't create user"
+        })
+        
+    }
+
+
+})
+
+module.exports = router
