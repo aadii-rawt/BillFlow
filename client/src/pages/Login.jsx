97b66b0601b66431309701bd2c591142c1eccd32
adityarawat4 <rawatadii060@gmail.com>
2025-01-20 16:39:20 +0530

    login flow

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,30 @@
 
-import React from "react";
+import React, { useState } from "react";
 import { HiOutlineMail } from "react-icons/hi";
 import { LuKeyRound } from "react-icons/lu";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:3000/users/login", {
+        email,
+        password
+      })
+      if (res?.status == 200) {
+        navigate("/")
+      }
+    } catch (error) {
+      console.log(error?.response?.data?.msg);
+      setError(error?.response?.data?.msg);
+    }
+  }
   return (
     <div className="flex flex-col lg:flex-row justify-center min-h-screen ">
       <div className=" w-1/2 px-6 lg:px-12">
@@ -13,7 +34,7 @@ const Login = () => {
             <h1 className="font-bold text-3xl">Login</h1>
             <p className="text-sm text-gray-600 my-2">See your growth and get more consulting support!</p>
           </div>
-          <form className="mt-5">
+          <form className="mt-5" onSubmit={handleLogin}>
             <div className="mb-4">
               <label className="block text-gray-700 font-medium text-sm mb-1" htmlFor="email">
                 Email*
@@ -23,10 +44,11 @@ const Login = () => {
                 <input
                   type="email"
                   id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className="w-full  bg-transparent px-1 py-2 outline-none "
-                  placeholder="adi@e.com"
+                  placeholder="Enter your email"
                 />
-
               </div>
             </div>
             <div className="mb-4">
@@ -38,12 +60,15 @@ const Login = () => {
                 <input
                   type="password"
                   id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   className="w-full bg-transparent px-1 py-2 outline-none"
                   placeholder="••••••••"
                 />
 
               </div>
             </div>
+            {error && <p className="text-red-500">{error}</p>}
             <button
               type="submit"
               className="w-full bg-[#5138EE] text-white font-bold py-2 px-4 my-3 rounded-3xl"
@@ -63,10 +88,9 @@ const Login = () => {
       </div>
 
       {/* right side */}
-      <div className="w-1/2  bg-[#5138EE] flex items-center justify-center" 
-      style={{backgroundImage : 'url("https://assets-global.website-files.com/61fd79cd8d028c476185f53e/61fd79cd8d028c53c685f5d5_Account%20Pattern.svg")'}}>
+      <div className="w-1/2  bg-[#5138EE] flex items-center justify-center"
+        style={{ backgroundImage: 'url("https://assets-global.website-files.com/61fd79cd8d028c476185f53e/61fd79cd8d028c53c685f5d5_Account%20Pattern.svg")' }}>
         <img src="https://assets-global.website-files.com/61fd79cd8d028c476185f53e/61fd79cd8d028c5bed85f643_Asset%20Cards.png" alt="Cloud Illustration" className="w-1/2" />
-
       </div>
     </div>
   );
