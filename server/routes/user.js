582448152e5384e9699e39f4806194e25c8e2723
adityarawat4 <rawatadii060@gmail.com>
2025-01-20 16:39:36 +0530

    c

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,43 +1,60 @@
 const express = require("express");
 const { Users } = require("../db");
-const router = express.Router()
+const router = express.Router();
 
-router.post("/signup", async (req,res) => {
-    console.log(req.body);
-    const email =  req.body.email
-    try {
-        const isUserExist = await Users.findOne({email})
-        console.log(isUserExist);
-        
-        if(isUserExist){
-            return res.status(409).send({
-                msg: "User with this email already exists."
-            });
-        }
+router.post("/signup", async (req, res) => {
+  console.log(req.body);
+  const email = req.body.email;
+  try {
+    const isUserExist = await Users.findOne({ email });
+    console.log(isUserExist);
 
-        await Users.create(
-            {
-                ...req.body
-            }
-        )
-
-        res.send({
-            msg : "user created successfully"
-        })
-
-    } catch (error) {
-        console.log(error);
-        res.send({
-            "msg" : "Error can't create user"
-        })
-        
+    if (isUserExist) {
+      return res.status(409).send({
+        msg: "User with this email already exists.",
+      });
     }
 
+    await Users.create({
+      ...req.body,
+    });
 
-})
+    res.send({
+      msg: "user created successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      msg: "Error can't create user",
+    });
+  }
+});
 
-router.post("/login", async (req,res) => {
-    console.log(req.body);
-})
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const isUserExist = await Users.findOne({
+      email: email,
+      password: password,
+    });
+    if (isUserExist) {
+      return res.status(200).send({
+        msg: "user login successfully",
+      });
+    }
+    return res.status(401).send({
+      msg: "Invalid email and password",
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      msg: "Someting wend wrong",
+    });
+  }
 
-module.exports = router
+  res.send({
+    msg: "login",
+  });
+});
+
+module.exports = router;
