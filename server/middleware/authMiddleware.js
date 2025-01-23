095866d541445b7f785687cd7f827ff73475773f
adityarawat4 <rawatadii060@gmail.com>
2025-01-23 12:17:40 +0530

    authmiddlware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,20 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const Auth = req.cookies
-  console.log(Auth);
-  
-  // if (!Authorization) {
-  //   return res.status(401).json({ message: "Unauthorized" });
-  // }
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-  // jwt.verify(Authorization, "ukfhnsdfkjh", (err, decoded) => {
-  //   if (err) {
-  //     return res.status(403).json({ message: "Invalid or expired token" });
-  //   }
-  //   console.log("this is data ", decoded);
-  next();
-  // });
+  try {
+    const decoded = jwt.verify(token, "adityarawat");
+    console.log("Decoded Token:", decoded);
+    req.userId = decoded._id
+    next();
+  } catch (error) {
+    console.error("Token verification failed:", error.message);
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
 };
 
 module.exports = authMiddleware;
