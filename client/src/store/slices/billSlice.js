60efa31e9bfbd775ce7ec6393c287423db44a22f
adityarawat4 <rawatadii060@gmail.com>
2025-01-30 21:23:01 +0530

    c

--- client/src/store/slices/billSlice.js
+++ client/src/store/slices/billSlice.js
@@ -16,6 +16,20 @@ const fakeBills = {
       billId: "w54e67rt8yu9oiugyjhvblkh",
       userId: "1278798764",
     },
+    {
+      items: [{ description: "Phone", quantity: 1, rate: 500, amount: 900 }],
+      billNumber: "#2345678",
+      uploadedLogo: "234578987yt",
+      FromAddress: "delhi",
+      vendorName: "amit",
+      date: "22/03/2024",
+      dueDate: "30/04/2024",
+      tax: 10,
+      note: "",
+      isPaid: "unpaid",
+      billId: "dfsdfjslkjflksh",
+      userId: "1278798764",
+    },
   ],
 };
 
