44526d493c78ab140a522d7fbee4eda3b65be10e
adityarawat4 <rawatadii060@gmail.com>
2025-02-02 19:12:28 +0530

    jwt decode in frontend for login session

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import { ScrollRestoration } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { setUser } from '../store/slices/stateSlice'
+import { jwtDecode } from "jwt-decode";
 
 function Layout() {
   // const dispatch = useDispatch();
@@ -15,6 +16,32 @@ function Layout() {
   //     dispatch(setUser(JSON.parse(storedUser))); // Restore user to Redux state
   //   }
   // }, [dispatch]);
+
+  const checkTokenExpire = () => {
+    const token = localStorage.getItem("authToken")
+    if (!token) return true
+    const realToken = token.split(" ")[1] // remove bearer
+    try {
+      const { exp } = jwtDecode(realToken)
+      if (exp * 1000 < Date.now()) {
+        // localStorage.removeItem("authToken");
+        console.log("token expire");
+        return true;
+      }
+      return false
+    } catch (error) {
+      return true;
+    }
+
+  }
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (checkTokenExpire()) {
+      navigate('/login')
+    }
+  }, [])
 
   return (
     <div className='flex min-h-screen'>
