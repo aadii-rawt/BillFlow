86ed8aed0926cfc12e8489dfa7c9f66f2d3f67f7
adityarawat4 <rawatadii060@gmail.com>
2025-02-02 20:59:25 +0530

    bill payment form

--- client/src/components/BillPayment.jsx
+++ client/src/components/BillPayment.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BillPreview from './BillPreview';
 
-function BillPayment({setPayment}) {
+function BillPayment({ setPayment, bill }) {
+
+    console.log(bill);
+
+    const [paymentData, setPaymentDate] = useState({
+        paymentId: crypto.randomUUID(),
+        billId: bill?.billId,
+        vendorId: bill?.vendorId,
+        amountPaid: bill?.totalDueAmount || 0,
+        paymentMethod: "",
+        paymentDate: "",
+        note: "",
+        createdAt: "",
+    })
+
+    const handleDataChange = (e) => {
+        const { name, value } = e.target
+        setPaymentDate((prev) => ({ ...prev, [name]: value }))
+    }
+
+    const handlePayment = (e) => {
+        e.preventDefault()
+
+        console.log(paymentData);
+
+    }
+
     return (
         <div className='min-h-screen border-l'>
             <div className='flex justify-between items-center p-[19px] border-b '>
@@ -11,19 +38,27 @@ function BillPayment({setPayment}) {
                     {/* <button className='text-red-500 text-2xl' onClick={() => dispatch(closeBillPreview())}><RxCross2 /></button> */}
                 </div>
             </div>
-            <div className='p-4'>
-                <form action="">
+            <form onSubmit={handlePayment}>
+                <div className='p-4'>
                     <div className='flex flex-col gap-1 '>
                         <label htmlFor="amount" className='text-[15px] text-red-600'>Payment Made </label>
                         <div>
-                            <input type="text" className='border-gray-500/50 border hover:border-blue-500 outline-none px-2 py-1 rounded-md' />
+                            <input type="text"
+                                name='amountPaid'
+                                value={paymentData?.amountPaid}
+                                onChange={handleDataChange}
+                                className='border-gray-500/50 border hover:border-blue-500 outline-none px-2 py-1 rounded-md' />
                         </div>
                     </div>
                     <div className='flex flex-col gap-1 '>
                         <label htmlFor="amount" className='text-[15px]'>Payment Mode </label>
                         <div>
                             {/* <input type="text" className='border-gray-500/50 border hover:border-blue-500 outline-none px-2 py-1 rounded-md' /> */}
-                            <select className='border-gray-500/50 border min-w-[200px] hover:border-blue-500 outline-none px-2 py-1 rounded-md'>
+                            <select
+                                name='paymentMethod'
+                                value={paymentData?.paymentMethod}
+                                onChange={handleDataChange}
+                                className='border-gray-500/50 border min-w-[200px] hover:border-blue-500 outline-none px-2 py-1 rounded-md'>
                                 <option value="">Cash</option>
                                 <option value="">UPI</option>
                                 <option value="">Bank</option>
@@ -35,25 +70,32 @@ function BillPayment({setPayment}) {
                         <div className='flex flex-col gap-1 '>
                             <label htmlFor="amount" className='text-[15px] text-red-600'>Payment Date*</label>
                             <div>
-                                <input type="date" className='min-w-[200px] border-gray-500/50 border hover:border-blue-500 outline-none px-2 py-1 rounded-md' />
+                                <input type="date"
+                                    name='paymentDate'
+                                    value={paymentData?.paymentDate}
+                                    onChange={handleDataChange}
+                                    className='min-w-[200px] border-gray-500/50 border hover:border-blue-500 outline-none px-2 py-1 rounded-md' />
                             </div>
                         </div>
-                        <div className='flex flex-col gap-1 '>
+                        {/* <div className='flex flex-col gap-1 '>
                             <label htmlFor="amount" className='text-[15px] text-red-600'>Payment Made </label>
                             <div>
-                                <input type="text" className='border-gray-500/50 border hover:border-blue-500 outline-none px-2 py-1 rounded-md' />
+                                <input type="text"
+                                    value={paymentData?.amountPaid}
+                                    onChange={(e) => setPaymentDate(e.target.value)}
+                                    className='border-gray-500/50 border hover:border-blue-500 outline-none px-2 py-1 rounded-md' />
                             </div>
-                        </div>
+                        </div> */}
                     </div>
-                </form>
-            </div>
+                </div>
 
-            <div className={`border-t p-4 flex gap-3 bottom-0 w-full fixed`}>
-                <button className='bg-blue-500 text-white px-2.5 py-1  rounded-md'>Save</button>
+                <div className={`border-t p-4 flex gap-3 bottom-0 w-full fixed`}>
+                    <button type='submit' className='bg-blue-500 text-white px-2.5 py-1  rounded-md'>Save</button>
 
-                <button onClick={() => setPayment(false)} className='bg-gray-100 px-2.5 py-1  rounded-md border' >Cancel</button>
-                {/* } */}
-            </div>
+                    <button onClick={() => setPayment(false)} className='bg-gray-100 px-2.5 py-1  rounded-md border' >Cancel</button>
+                    {/* } */}
+                </div>
+            </form>
         </div>
     )
 }
