396318b3309d8782bfdd5a8a7a60533a45e21d22
adityarawat4 <rawatadii060@gmail.com>
2025-02-08 20:23:43 +0530

    user profile animation

--- client/src/components/ProfileRightSidebar.jsx
+++ client/src/components/ProfileRightSidebar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { RxCross2 } from "react-icons/rx";
+import { useDispatch } from 'react-redux';
+import { handleUserProfile } from '../store/slices/stateSlice';
 
 function ProfileRightSidebar() {
+    const dispatch = useDispatch()
     return (
-        <div className='absolute flex justify-end h-full top-16 right-0 bg-black/50 border-l w-full'>
-
-            <div className='w-[25%] bg-[#F9F9FB]'>
+        <div className='absolute flex overflow-hidden justify-end h-full top-16 right-0 bg-gray-900/50 border-l w-full'>
+            <div className='w-[28%] bg-[#F9F9FB] animate-slide-right'>
                 <div className='flex justify-between items-center p-4'>
                     <div className='flex gap-2'>
-                        <div className='w-12 h-12 bg-red-500 rounded-md'>
+                        <div className='w-12 h-12 rounded-md'>
+                            <img src="/img/image.png" alt="" />
                         </div>
                         <div>
                             <h1 className='font-medium'>Aditya Rawat</h1>
@@ -15,10 +19,14 @@ function ProfileRightSidebar() {
                         </div>
                     </div>
                     <div>
-                        <button></button>
+                        <button onClick={() => dispatch(handleUserProfile())} className='text-red-500 text-2xl'><RxCross2 /></button>
                     </div>
                 </div>
 
+                <div className='flex justify-between items-center px-5 text-[15px] my-5 border-y py-3'>
+                    <p className='text-blue-500'>My Account</p>
+                    <button className='text-red-500'>Sign Out</button>
+                </div>
             </div>
         </div>
     )
