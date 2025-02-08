3abb24f2e1f4b75d53c11eedeb65673f47e79bc0
adityarawat4 <rawatadii060@gmail.com>
2025-02-08 19:52:47 +0530

    user profile right side

---
+++ client/src/components/ProfileRightSidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ProfileRightSidebar() {
+    return (
+        <div className='absolute top-0 right-0 bg-white border-l w-[70%]'>
+            <div className='flex justify-between items-center p-4'>
+                <div>
+                    <h1 className='text-2xl'>User Profile</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileRightSidebar
