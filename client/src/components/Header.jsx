d9256b071369d7fa206aaeb9a5b4e1322ceab560
adityarawat4 <rawatadii060@gmail.com>
2025-02-08 20:39:27 +0530

    header link of setting.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { IoIosSearch } from "react-icons/io";
 import { useDispatch } from 'react-redux'
 import { handleUserProfile, setBillPreview } from '../store/slices/stateSlice';
+import { Link } from 'react-router-dom';
 function Header() {
   const dispatch = useDispatch()
   return (
@@ -12,7 +13,8 @@ function Header() {
           <input type="text" placeholder='Search in Bills' className='outline-none bg-transparent text-white px-2 py-0.5 border-l !border-gray-600' />
         </div>
       </div>
-      <div>
+      <div className='flex gap-5 items-center'>
+        <Link to='/setting' className='text-white '>setting</Link>
         <div onClick={() => dispatch(handleUserProfile(true))} className='w-10 h-10 cursor-pointer rounded-full bg-green-600 text-white flex items-center justify-center font-medium text-xl'>A</div>
       </div>
     </div>
