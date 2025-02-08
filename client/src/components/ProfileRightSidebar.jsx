da89bf8c5ad1791ce03d9948bf9e51d428a9f19d
adityarawat4 <rawatadii060@gmail.com>
2025-02-08 20:04:30 +0530

    user data

--- client/src/components/ProfileRightSidebar.jsx
+++ client/src/components/ProfileRightSidebar.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 
 function ProfileRightSidebar() {
     return (
-        <div className='absolute top-0 right-0 bg-white border-l w-[70%]'>
-            <div className='flex justify-between items-center p-4'>
-                <div>
-                    <h1 className='text-2xl'>User Profile</h1>
+        <div className='absolute flex justify-end h-full top-16 right-0 bg-black/50 border-l w-full'>
+
+            <div className='w-[25%] bg-[#F9F9FB]'>
+                <div className='flex justify-between items-center p-4'>
+                    <div className='flex gap-2'>
+                        <div className='w-12 h-12 bg-red-500 rounded-md'>
+                        </div>
+                        <div>
+                            <h1 className='font-medium'>Aditya Rawat</h1>
+                            <p className='text-gray-500'>rawatadii060@gmail.com</p>
+                        </div>
+                    </div>
+                    <div>
+                        <button></button>
+                    </div>
                 </div>
+
             </div>
         </div>
     )
