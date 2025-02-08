6918c64e78e3d0f38c0bf823808844826574cdb3
adityarawat4 <rawatadii060@gmail.com>
2025-02-08 19:52:36 +0530

    handle user profile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { IoIosSearch } from "react-icons/io";
-
+import { useDispatch } from 'react-redux'
+import { handleUserProfile, setBillPreview } from '../store/slices/stateSlice';
 function Header() {
+  const dispatch = useDispatch()
   return (
     <div className='bg-[#21263c]  px-5 py-3 flex items-center justify-between'>
       <div>
@@ -11,7 +13,7 @@ function Header() {
         </div>
       </div>
       <div>
-        <div className='w-10 h-10 cursor-pointer rounded-full bg-green-600 text-white flex items-center justify-center font-medium text-xl'>A</div>
+        <div onClick={() => dispatch(handleUserProfile(true))} className='w-10 h-10 cursor-pointer rounded-full bg-green-600 text-white flex items-center justify-center font-medium text-xl'>A</div>
       </div>
     </div>
   )
