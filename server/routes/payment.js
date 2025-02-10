9c3f1f3792c249eb97c11705d0cfc0736ea5d9c8
adityarawat4 <rawatadii060@gmail.com>
2025-02-10 13:05:03 +0530

    payment logic

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
+const { Payements } = require("../db");
 
 router.post("/createPayment", authMiddleware, async (req, res) => {
+  const userId = req.userId;
+  try {
+    // let userVendors = await Pay.findOne({ _id: userId });
+    let userPayments = await Payements.findOne({ _id: userId });
+
+    if (!userPayments) {
+      userPayments = await Payements.create({
+        _id: userId,
+        payements: [],
+      });
+    }
+
+    userPayments.payments.push({
+      ...req.body,
+    });
+
+    await userPayments.save();
+
+    res.json({ msg: "payment made successfully" });
+  } catch (error) {
+    console.log(error);
     
+    res.json({ msg: "Error making payment" });
+  }
 });
 
 module.exports = router;
