32a8c8c928914384baa410ac6bd7ce032679a25f
adityarawat4 <rawatadii060@gmail.com>
2025-02-10 13:59:09 +0530

    commit

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,31 +1,51 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
-const { Payements } = require("../db");
+const { Payements, Bills } = require("../db");
 
 router.post("/createPayment", authMiddleware, async (req, res) => {
   const userId = req.userId;
+  const { billId } = req.body;
   try {
     // let userVendors = await Pay.findOne({ _id: userId });
-    let userPayments = await Payements.findOne({ _id: userId });
 
-    if (!userPayments) {
-      userPayments = await Payements.create({
-        _id: userId,
-        payements: [],
-      });
+    let userBills = await Bills.findOne({ _id: userId });
+
+    if (!userBills) {
+      return res.status(404).json({ msg: "No bills found for this user" });
     }
 
-    userPayments.payments.push({
-      ...req.body,
-    });
+    const billIndex = userBills.bills.findIndex(
+      (bill) => bill?.billId === billId
+    );
+    if (billIndex === -1) {
+      return res.status(404).json({ msg: "Bill not found" });
+    }
 
-    await userPayments.save();
+    userBills.bills[billIndex].isPaid = "paid" || "Paid"; // Default to "Paid"
+
+    await userBills.save();
+
+    // store payment
+    // let userPayments = await Payements.findOne({ _id: userId });
+
+    // if (!userPayments) {
+    //   userPayments = await Payements.create({
+    //     _id: userId,
+    //     payements: [],
+    //   });
+    // }
+
+    // userPayments.payments.push({
+    //   ...req.body,
+    // });
+
+    // await userPayments.save();
 
     res.json({ msg: "payment made successfully" });
   } catch (error) {
     console.log(error);
-    
+
     res.json({ msg: "Error making payment" });
   }
 });
